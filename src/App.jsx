585f41986e860df3267e584a2e3ed512fc7e2e2e
Donn Neufeld <donn.neufeld@gmail.com>
2023-04-18 10:19:44 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Task Bender</h1>
+      <h2>Put your todo's on your calendar</h2>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
